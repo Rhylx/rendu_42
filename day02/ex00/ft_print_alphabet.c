d45388a6752de7d8d238e5f46cecd36b16cdb804
Rhylx <redbull11570@gmail.com>
2018-08-07 00:51:02 +0200

    bonne fonction

---
+++ day02/ex00/ft_print_alphabet.c
@@ -0,0 +1,17 @@
+# include <unistd.h>
+
+void ft_putchar(char c);
+
+void ft_print_alphabet()
+{
+	char c;
+
+	c = 'a';
+	while (c<='z')
+{
+
+ft_putchar(c);
+c++;
+
+}
+}
---
+++ day02/ex00/main.c
@@ -0,0 +1,16 @@
+# include <unistd.h>
+
+void ft_print_alphabet(void);
+
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+
+}
+
+int main()
+{
+
+  ft_print_alphabet();
+   return(123);
+}
